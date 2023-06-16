d4cb31f0a81adbf108ebc58099b9e09787331824
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-06-16 14:16:58 +0900

    Initial commit

---
+++ src/main/java/com/springboot/test/data/dto/ProductDto.java
@@ -0,0 +1,14 @@
+package com.springboot.test.data.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ProductDto {
+    private String name;
+    private int price;
+    private int stock;
+}
---
+++ src/main/java/com/springboot/test/data/dto/impl/ProductResponseDto.java
@@ -0,0 +1,17 @@
+package com.springboot.test.data.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProductResponseDto {
+    private Long number;
+    private String name;
+    private int price;
+    private int stock;
+}
