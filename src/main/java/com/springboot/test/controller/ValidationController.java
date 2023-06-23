dc28f8fd673fc6d640a3f7097b9165d315980ff0
sahngwoon_lee <sahngwoon.lee@llsollu.com>
2023-06-23 10:10:50 +0900

    Swagger & /validation/valid

---
+++ src/main/java/com/springboot/test/controller/ValidationController.java
@@ -0,0 +1,28 @@
+package com.springboot.test.controller;
+
+import com.springboot.test.data.dto.ValidRequestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/validation")
+public class ValidationController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ValidationController.class);
+
+    @PostMapping("/valid")
+    public ResponseEntity<String> checkValidationByValid(
+            @Valid @RequestBody ValidRequestDto validRequestDto){
+            LOGGER.info( validRequestDto.toString());
+            return ResponseEntity.status(HttpStatus.OK).body(validRequestDto.toString());
+    }
+
+
+}
